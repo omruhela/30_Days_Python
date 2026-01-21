70c8c544dfceec18f6075843c6de0ec962db1436
omruhela <123805730+omruhela@users.noreply.github.com>
2026-01-21 21:28:37 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,34 @@
+# Here we understand about the variables in python...
+num=30 
+name="omruhela"
+float_number=20.67
+
+#Q1 = Print a name in the terminal 
+name="Omruhela"
+print(name)
+
+
+#Q2 = Print two different numbers and add them...
+num_1=20
+num_2=30
+total=20+30
+print(total)
+
+
+#Q3 = Write a program to find remainder when a number is divided by 2.
+num=20
+print(num%2)
+
+
+#Q4 = Write a program to find the average of three numbers...
+num_1=int(input("Enter the first number : "))
+num_2=int(input("Enter the second number : "))
+num_3=int(input("Enter the third number : "))
+average=(num_1+num_2+num_3)/3
+print(average)
+
+
+#Q5 = Write a program to find the square of a number and input taken by the user
+num=int(input("Enter the number : "))
+square=num**2
+print(square)
